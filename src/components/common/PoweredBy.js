4c9d93bebed1a88af80de04f49bd11b5bec44104
Ramon Guilherme <ramon.nascimento@cs.udf.edu.br>
2024-07-24 15:47:24 -0300

    dele file

--- src/components/common/PoweredBy.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import AppContext from '../../AppContext';
-        /* eslint-disable i18next/no-literal-string */
-        );
-        /* eslint-enable i18next/no-literal-string */
-    }
-}
