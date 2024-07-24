f8af7eae24c8ba9a3f8c45130d30fadc0356166f
Ramon Guilherme <ramon@ramonguilherme.com.br>
2024-07-24 15:56:45 -0300

    Update PoweredBy.js

--- src/components/common/PoweredBy.js
+++ src/components/common/PoweredBy.js
@@ -6,7 +6,6 @@ export default class PoweredBy extends React.Component {
     static contextType = AppContext;
 
     render() {
-        // Note: please do not wrap "Powered by Ghost" in the translation function, as we don't
-        // want it to be translated
-        /* eslint-disable i18next/no-literal-string */
+
+    }
 }
